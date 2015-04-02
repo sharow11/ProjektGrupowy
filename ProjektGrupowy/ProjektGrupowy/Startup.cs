b91ef690a9046f1aeb4e02385c611ddddc529e3f
sharow11 <sharow1234@gmail.com>
2015-04-02 22:25:36 +0200

    First

---
+++ ProjektGrupowy/ProjektGrupowy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjektGrupowy.Startup))]
+namespace ProjektGrupowy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
