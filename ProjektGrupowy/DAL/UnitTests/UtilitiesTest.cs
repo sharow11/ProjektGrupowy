777fc688282c7a7d5e0a367e3ed4e24dfeb4b338
Domi <ciasteczko.i.mleczko@gmail.com>
2015-08-10 23:43:36 +0200

    zmieniłam configi zeby podlinkować sqlite
    potem możemy znaleźć dll 64 bit, teraz mi się nie chce

---
+++ ProjektGrupowy/DAL/UnitTests/UtilitiesTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.IO;
+
+namespace DAL.UnitTests
+{
+    [TestFixture]
+    class UtilitiesTest
+    {
+        [TestCase("Create Database")]
+        public void Test(string name)
+        {
+            string newFile = DatabaseFileUtilities.Filename;
+            Assert.That(null != newFile);
+
+            DatabaseFileUtilities.CreateFile();
+            Assert.That(File.Exists(newFile));
+        }
+    }
+}
